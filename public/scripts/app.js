c7b55803ac583ece72b1b6eb25f84e153b14ce0b
David Ragsdale <me@davidjragsdale.com>
2019-06-19 13:42:45 +0100

    Show cached forecast until network responds

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -163,7 +163,21 @@ function getForecastFromNetwork(coords) {
  */
 function getForecastFromCache(coords) {
   // CODELAB: Add code to get weather forecast from the caches object.
-
+  if (!('caches' in window)) {
+    return null;
+  }
+  const url = `${window.location.origin}/forecast/${coords}`;
+  return caches.match(url)
+    .then((response) => {
+      if (response) {
+        return response.json();
+      }
+      return null;
+    })
+    .catch((err) => {
+      console.error('Error getting data from cache', err);
+      return null;
+    });
 }
 
 /**
@@ -198,6 +212,10 @@ function updateData() {
     const location = weatherApp.selectedLocations[key];
     const card = getForecastCard(location);
     // CODELAB: Add code to call getForecastFromCache
+    getForecastFromCache(location.geo)
+      .then((forecast) => {
+        renderForecast(card, forecast);
+      });
 
     // Get the forecast data from the network.
     getForecastFromNetwork(location.geo)
